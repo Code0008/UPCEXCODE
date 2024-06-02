19da388bc00da71df0446bd3a23aa48db8d9caef
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-02 00:16:54 -0500

    Update PRINCE LIBRARY.cpp

--- PRINCE LIBRARY/PRINCE LIBRARY.cpp
+++ PRINCE LIBRARY/PRINCE LIBRARY.cpp
@@ -1,54 +1,22 @@
 // PROPUESTA 008 PARA SISSOFT-UPC
-// 01/06/2024 15:42 COMENZANDO PROYECTO
+// 01/06/2024 22:08 INCIO
+// 02/06/2024 00:16 ACTUALIZANDO
 
 #include <iostream>
 #include <string>
 #include <stdio.h>
 #include <conio.h>
-
+#include "INFO_DE_MENUS.h"
+#include "FUNCIONALIDADES.h"
+//#include "TESTS.h"
 using namespace std;
-
-
-
-struct direccion_usuario {
-	string DIRECCION;
-	string DISTRITO;
-	string PROVINCIA;
-	string CASA_DEPARTAMENTO;
-};
-
-struct USUARIO{
-	string DNI;
-	string nombre_apellido;
-	int EDAD;
-	char SEXO;	
-	direccion_usuario direccion;
-	string celular;
-};
-
-
-struct LIBRO {
-	int codigo_libro;
-	string tipo_libro;
-	string titutlo_libro;
-};
-
-
-
-void capture_time() {
-	tm tiempo;
-	time_t timestap;
-	time(&timestap);
-	localtime_s(&tiempo, &timestap);
-	cout << tiempo.tm_year+1900;
-	cout <<"/"<< tiempo.tm_mon;
-	cout << "/" << tiempo.tm_mday+5;		
-}
-
-
-
-
 int main() {
-	//capture_time();
+	while (true) {
+		int contador_usuario=0;
+		menu_principal();
+		cout << "\t" << capture_time();
+		obtener_informacion_usuario(contador_usuario);
+		if (respuesta_continuar() != true) { break;}
+	}
 
 }
