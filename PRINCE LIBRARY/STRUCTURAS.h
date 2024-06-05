1ba65571eb75a22d90a92acd58caa49c98185cb8
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-05 15:34:56 -0500

    Update STRUCTURAS.h

--- PRINCE LIBRARY/STRUCTURAS.h
+++ PRINCE LIBRARY/STRUCTURAS.h
@@ -30,3 +30,38 @@ struct LIBRO {
 	string titutlo_libro;
 };
 
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+
+struct LIBRO {
+	int codigo_libro;
+	string tipo_libro;
+	string titutlo_libro;
+};
+
+struct direccion_usuario {
+	string DIRECCION="void";
+	string DISTRITO="void";
+	string PROVINCIA="LIMA";
+	string CASA_DEPARTAMENTO="void";
+};
+
+struct USUARIO {
+	int userID=0;
+	string DNI="void";
+	string NOMBRE_APELLIDO;
+	int EDAD;
+	char SEXO;
+	direccion_usuario direccion;
+	string CELULAR="void";
+	string hora_operacion="void";
+	string correo="void";
+	int cantidad_libros_prestado=0;
+	int cantidad_libros_devueltos=0;
+	LIBRO* libros_retirados = new LIBRO[100];
+	LIBRO* libros_devueltos = new LIBRO[100];
+};
+
