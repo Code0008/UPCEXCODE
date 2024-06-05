b69c18770521a4aefdf71d4b9f56462edd99e975
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-05 15:35:21 -0500

    Update PRINCE LIBRARY.cpp

--- PRINCE LIBRARY/PRINCE LIBRARY.cpp
+++ PRINCE LIBRARY/PRINCE LIBRARY.cpp
@@ -1,35 +1,30 @@
 
 #include <iostream>
-#include <string>
 #include <stdio.h>
 #include <conio.h>
 #include "INFO_DE_MENUS.h"
 #include "FUNCIONALIDADES.h"
+#include <string>
 //#include "TESTS.h"
 #include "COLORAMA.h"
 using namespace std;
 int main() {
 	RESET;
 	int contador_usuario = 0;
-	int seleccion = 0;
+	string seleccion;
 	while (true) {
 		menu_login();
-		cout << "\t"; estetica(30, '*');
-		cout << "\t\t\t" << capture_time() << endl;
-		cout << "\t"; estetica(30, '*');
 		obtener_informacion_usuario(contador_usuario);
 		contador_usuario++;
 		system("cls");
-
-		menu_principal();
+		menu_principal(); cin.ignore();
 		while (true) {
-			seleccion = 0;
 			cout << "[+] INGRESE SELECCION: ";
-			cin >> seleccion;
-			if (seleccion == 1 || seleccion == 2 || seleccion == 3) { break; }
-			else { cout << "\t[!]Ingrese una seleccion valida\n";   continue; }
+			getline(cin, seleccion);
+			if (verif_entero(seleccion)) { if (stoi(seleccion) == 1 || stoi(seleccion) == 2 || stoi(seleccion) == 3) { break; } else  { cout << RED << "\t[!]" << RESET << ORANGE << "Ingrese una opcion valida!\n" << RESET; continue; } }
+			else { cout << RED << "\t[!]" << RESET << ORANGE<<"Ingrese una opcion valida!\n"<< RESET; }
 		}
-		switch (seleccion)
+		switch (stoi(seleccion))
 		{
 		case 1:
 			system("cls");
@@ -43,8 +38,11 @@ int main() {
 
 		cin.ignore();
 	}
+	system("cls");
 	see_data(2);
 	RESET;
-
+	/*/cin.ignore();
+	menu_prestamo();
+	obtener_informacion_prestamo();*/
+	
 }
-
