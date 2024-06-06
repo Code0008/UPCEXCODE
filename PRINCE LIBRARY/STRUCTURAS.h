9fc75bbb1d00bdf397438191c7c29beef6991b49
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-05 19:26:47 -0500

    Update STRUCTURAS.h

--- PRINCE LIBRARY/STRUCTURAS.h
+++ PRINCE LIBRARY/STRUCTURAS.h
@@ -1,34 +1,3 @@
-#include <stdio.h>
-#include <conio.h>
-
-using namespace std;
-
-
-
-struct direccion_usuario {
-	string DIRECCION;
-	string DISTRITO;
-	string PROVINCIA="LIMA";
-	string CASA_DEPARTAMENTO;
-};
-
-struct USUARIO {
-	int userID;
-	string DNI;
-	string NOMBRE_APELLIDO;
-	int EDAD;
-	char SEXO;
-	direccion_usuario direccion;
-	string CELULAR;
-	string hora_operacion;
-};
-
-
-struct LIBRO {
-	int codigo_libro;
-	string tipo_libro;
-	string titutlo_libro;
-};
 
 #include <stdio.h>
 #include <conio.h>
@@ -43,25 +12,27 @@ struct LIBRO {
 };
 
 struct direccion_usuario {
-	string DIRECCION="void";
-	string DISTRITO="void";
-	string PROVINCIA="LIMA";
-	string CASA_DEPARTAMENTO="void";
+	string DIRECCION = "void";
+	string DISTRITO = "void";
+	string PROVINCIA = "LIMA";
+	string CASA_DEPARTAMENTO = "void";
 };
 
 struct USUARIO {
-	int userID=0;
-	string DNI="void";
+	int userID = 0;
+	string DNI = "void";
 	string NOMBRE_APELLIDO;
 	int EDAD;
 	char SEXO;
 	direccion_usuario direccion;
-	string CELULAR="void";
-	string hora_operacion="void";
-	string correo="void";
-	int cantidad_libros_prestado=0;
-	int cantidad_libros_devueltos=0;
-	LIBRO* libros_retirados = new LIBRO[100];
-	LIBRO* libros_devueltos = new LIBRO[100];
-};
+	string CELULAR = "void";
+	string hora_operacion = "void";
+	string correo = "void";
+	string** libros_tratados = new string*[100];
 
+	/*
+		| codigo | hora_tratado | operacion que se realizo(P/D) | Verificador_coompleto_operacion |
+		1 fila  3 columnas
+
+	*/
+};
