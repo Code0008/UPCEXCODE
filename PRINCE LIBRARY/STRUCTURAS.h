c4dc2f1a02f643ca2299fce6b2af647f414d985b
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-08 10:45:30 -0500

    Update STRUCTURAS.h

--- PRINCE LIBRARY/STRUCTURAS.h
+++ PRINCE LIBRARY/STRUCTURAS.h
@@ -9,6 +9,7 @@ struct LIBRO {
 	int codigo_libro;
 	string tipo_libro;
 	string titutlo_libro;
+	bool prestado;
 };
 
 struct direccion_usuario {
@@ -29,10 +30,25 @@ struct USUARIO {
 	string hora_operacion = "void";
 	string correo = "void";
 	string** libros_tratados = new string*[100];
-
+	bool tiene_sancion=false;
+	string tiempo_sancion = "NO PRESENTA";
 	/*
 		| codigo | hora_tratado | operacion que se realizo(P/D) | Verificador_coompleto_operacion |
 		1 fila  3 columnas
 
+	*/
+};
+
+struct ADMINISTRADOR {
+
+	string nombre_adminsitrador;
+	string password;
+	string hora_acceso;
+	string correo_contacto;
+	bool see_reportes = false;
+	bool adm_users = false;
+	/*
+	Ver reportes | Desbloquear/Bloquear usuarios | 
+	|     V/F    | V/F							 |
 	*/
 };
